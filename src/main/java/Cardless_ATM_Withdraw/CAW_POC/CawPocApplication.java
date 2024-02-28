a8652d8e1be9faaa61caba39a1114eb7ddb4edc8
bishnupada81 <ayerish9596@yahoo.com>
2024-02-28 11:24:57 +0530

    first commit

---
+++ src/main/java/Cardless_ATM_Withdraw/CAW_POC/CawPocApplication.java
@@ -0,0 +1,13 @@
+package Cardless_ATM_Withdraw.CAW_POC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CawPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CawPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Cardless_ATM_Withdraw/CAW_POC/CawPocApplicationTests.java
@@ -0,0 +1,13 @@
+package Cardless_ATM_Withdraw.CAW_POC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CawPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
